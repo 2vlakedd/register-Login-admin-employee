0186ce6cd69f183f6b2f98088098c9c0b99a5aa2
2vlaked Senpai <39975168+2vlakedd@users.noreply.github.com>
2021-12-01 09:06:45 +0800

    Update login.php

--- php-basic-conection-main/login.php
+++ php-basic-conection-main/login.php
@@ -12,7 +12,8 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
 require_once "connection.php";
  
 // Define variables and initialize with empty values
-$username = $password = "";
+
+$username = $password  ="";
 $username_err = $password_err = $login_err = "";
  
 // Processing form data when form is submitted
@@ -31,19 +32,20 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     } else{
         $password = trim($_POST["password"]);
     }
+	
     
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
-        
+        $sql = "SELECT id,role,firstname,lastname,middlename,created_at, username, password  FROM users WHERE username = ?";
+       //  $query = "SELECT id, username, password ,role FROM users id,username,role = ?";
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_username);
             
             // Set parameters
             $param_username = $username;
-            
+          
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Store result
@@ -52,7 +54,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 // Check if username exists, if yes then verify password
                 if(mysqli_stmt_num_rows($stmt) == 1){                    
                     // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    mysqli_stmt_bind_result($stmt, $id,$role,$firstname,$lastname,$middlename,$createdat, $username, $hashed_password);
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
                             // Password is correct, so start a new session
@@ -61,8 +63,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             // Store data in session variables
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;                            
-                            
+                            $_SESSION["username"] = $username;     
+                             $_SESSION["role"] = $role;   
+							$_SESSION["created_at"] = $createdat;   
+$_SESSION["lastname"] = $lastname; 	
+$_SESSION["middlename"] = $middlename; 						 
+                            $_SESSION["firstname"] = $firstname; 
+                            							
+							
                             // Redirect user to welcome page
                             header("location: welcome.php");
                         } else{
